6fa3ad6058078917a97f4acd40125dc582f772a2
Milan <milanm1120@gmail.com>
2022-02-10 19:01:33 -0500

    create skeleton login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
+    state = {
+        email: '',
+        password: ''
+    }
+
   render() {
     return (
-      <div>Login</div>
+      <div className='login-container'>
+          <h3>LOGIN</h3>
+          <hr />
+          <form>
+              <label>Email: </label>
+              <input type='email' name='email' placeholder='example@abc.com' value={this.state.email} />
+              <label>Password: </label>
+              <input type='password' name='password' value={this.state.password} required />
+
+              <input type='submit' value='login' id='login-btn' />
+          </form>
+      </div>
     )
   }
 }
