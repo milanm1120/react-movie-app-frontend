d307c131f53cd8223cb21ce333fd8ca408e2ff1a
Milan <milanm1120@gmail.com>
2022-02-11 12:08:13 -0500

    rendering movie information

--- src/components/MovieItems.js
+++ src/components/MovieItems.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 
+const imageUrlComponent = 'https://image.tmdb.org/t/p/w342';
+
 class MovieItems extends Component {
   render() {
     return (
       <div>
-          <img src={this.props.movie.poster_path} />
-          <h3>{this.props.movie.title}</h3>
+          <hr/>
+            <img
+                src={imageUrlComponent + this.props.movie.poster_path}
+                alt={this.props.movie.title + " Image"}
+                height='150px' width='100px%'
+            />
+            <h3>{this.props.movie.title}</h3>
+            <p>{this.props.movie.overview}</p>
+            <p>Release Date: {this.props.movie.release_date}</p>
+            <p>Ratings: {this.props.movie.vote_average}</p>
+            
       </div>
     )
   }
