bef116f4013e212cf82b73c1b039ee8e1a23285d
Milan <milanm1120@gmail.com>
2022-02-10 19:31:06 -0500

    create handleClick and handleSubmit for LOGIN form, incomplete

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,16 +6,27 @@ class Login extends Component {
         password: ''
     }
 
+    handleChange = (event) => {
+        this.setState ({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        /* incomplete */
+    }
+
   render() {
     return (
       <div className='login-container'>
           <h3>LOGIN</h3>
           <hr />
-          <form>
+          <form onSubmit={(event) => this.handleSubmit(event)} >
               <label>Email: </label>
-              <input type='email' name='email' placeholder='example@abc.com' value={this.state.email} />
+              <input type='email' name='email' placeholder='example@abc.com' value={this.state.email} onChange={(event) => this.handleChange(event)} />
               <label>Password: </label>
-              <input type='password' name='password' value={this.state.password} required />
+              <input type='password' name='password' value={this.state.password} onChange={(event) => this.handleChange(event)} required />
 
               <input type='submit' value='login' id='login-btn' />
           </form>
